52953b9e9376a350e850f220798d47c2525d54a3
YYWO <112981137+YYWO@users.noreply.github.com>
2022-12-17 18:24:56 +0800

    Add files via upload

--- shell/All.sh
+++ shell/All.sh
@@ -1,28 +1,86 @@
 #!/bin/bash
-green(){
-    echo -e "\033[32m\033[01m$1\033[0m"
-}
-clear
-green "
-—————————————————————————————————————————————————————————————
-               欢迎使用Linux便民工具箱
-—————————————————————————————————————————————————————————————
-"
-
-[[ $(id -u) != 0 ]] && green "检测到当前环境非root用户，请先执行sudo su切换root用户执行脚本" && exit 1
-
-green "1.一键DD系统"
-green "2.一键换源安装docker"
-green "3.一键cloudflare内网穿透"
-
-read -p "请输入序号: " yn
- if [[ $yn == "1" ]];then
-  bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/YYWO/practical/main/shell/InstallNET.sh') -u 20.04 -v 64 -a -firmware -p practical
- elif [[ $yn == "2" ]];then
-  bash <(curl -sSL https://raw.githubusercontent.com/YYWO/practical/main/shell/DockerInstallation.sh)
- elif [[ $yn == "3" ]];then
-  bash <(curl -sSL https://raw.githubusercontent.com/YYWO/practical/main/shell/Tunnel.sh)
-else
-   green "输入有误，脚本退出，如有需要请重新运行"
-   exit 1
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "错误：本脚本需要 root 权限执行。" 1>&2
+    exit 1
 fi
+
+welcome() {
+    echo "一键脚本出现任何问题请自行安装！ "
+    echo ""
+    echo "欢迎使用Linux便民工具"
+}
+
+apt_update() {
+    echo "正在优化 apt-get . . ."
+    apt-get install sudo -y >>/dev/null 2>&1
+    apt-get update >>/dev/null 2>&1
+}
+
+apt_bash_check() {
+    echo "正在检查 bash 安装情况 . . ."
+    if command -v bash >>/dev/null 2>&1; then
+        echo "bash 似乎存在, 继续 . . ."
+    else
+        echo "bash 未安装在此系统上，正在进行安装"
+        apt-get install bash -y >>/dev/null 2>&1
+    fi
+}
+
+apt_wget_check() {
+    echo "正在检查 wget 安装情况 . . ."
+    if command -v wget >>/dev/null 2>&1; then
+        echo "wget 似乎存在, 继续 . . ."
+    else
+        echo "wget 未安装在此系统上，正在进行安装"
+        apt-get install wget -y >>/dev/null 2>&1
+    fi
+}
+
+DD() {
+    echo "一键DD系统"
+    echo "请选择系统 Ubuntu输入u debian输入d"
+    read $D
+    echo "你选择的系统是$D,请选择系统版本号"
+    read $DD
+    echo "请输入系统密码"
+    read $password
+    echo "系统为$D$DD,账号为root,密码为$password"
+    bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/YYWO/practical/main/shell/InstallNET.sh') -$D $DD -v 64 -a -firmware -p $password
+    
+}
+
+DD() {
+    echo "一键换源安装docker"
+    bash <(curl -sSL https://raw.githubusercontent.com/YYWO/practical/main/shell/DockerInstallation.sh)
+}
+
+shon_online() {
+    echo "一键脚本出现任何问题请手动安装"
+    echo ""
+    echo ""
+    echo "请选择您需要进行的操作:"
+    echo "  0) 退出脚本"
+    echo "  1) 一键DD系统"
+    echo "  2) 一键换源安装docker"
+    echo "  3) 一键cloudflare内网穿透"
+    echo "  4) 安装/更新 最新长期支持版nodejs"
+    echo "  5) 安装/更新 最新当前发布版nodejs"
+    echo "  6) 安装/更新 指定版本nodejs"
+    echo "  7) 强制更新nodejs"
+    echo ""
+    echo -n "请输入编号: "
+    read N
+    case $N in
+    1) DD ;;
+    2) cleanup ;;
+    3) reinstall ;;
+    4) cleansession ;;
+    5) stop_pager ;;
+    6) start_pager ;;
+    7) restart_pager ;;
+    0) exit ;;
+    *) echo "Wrong input!" ;;
+    esac
+}
+shon_online
