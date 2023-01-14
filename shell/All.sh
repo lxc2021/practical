0db6c9e6493be9dbba68eca77585161a0a86f59b
lxc2021 <89238958+lxc2021@users.noreply.github.com>
2023-01-14 17:04:53 +0800

    Update All.sh

--- shell/All.sh
+++ shell/All.sh
@@ -203,7 +203,7 @@ shon_online() {
     echo "  4) 安装/更新nodejs"
     echo "  5) docker安装filebrowser"
     echo "  6) Frp一键脚本"  
-   # echo "  7) mzzsfy代理池搭建"  
+    echo "  7) mzzsfy代理池搭建"  
     echo ""
     echo -n "请输入编号: "
     read N
